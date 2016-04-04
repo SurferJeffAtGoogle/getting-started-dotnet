562a5627f52ad3b9524ffd7c975b37d212995d5c
Jeffrey Rennie <rennie@google.com>
2016-04-04 16:26:32 -0700

    worker-mvc builds and runs with warning message about conflicting assemblies.

---
+++ aspnet/5-pubsub/worker-mvc/App_Start/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(worker_mvc.Startup))]
+
+namespace worker_mvc
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
