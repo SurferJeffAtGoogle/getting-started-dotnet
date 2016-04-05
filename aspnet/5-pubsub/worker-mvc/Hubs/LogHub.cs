c79f6603fea63a8adb5655caba412242cd33cc24
Jeffrey Rennie <rennie@google.com>
2016-04-05 16:04:45 -0700

    LogHub is empty.

--- aspnet/5-pubsub/worker-mvc/Hubs/LogHub.cs
+++ aspnet/5-pubsub/worker-mvc/Hubs/LogHub.cs
@@ -8,18 +8,5 @@ namespace worker_mvc.Hubs
 {
     public class LogHub : Hub
     {
-        private readonly LogTicker _logTicker;
-
-        public LogHub()
-        {
-            _logTicker = LogTicker.Instance;
-        }
-
-        public void LogVerbose(string message) => Clients.All.logVerbose(message);
-
-        public void LogError(string message, Exception e)
-        {
-            Clients.All.logError(message, e.ToString());
-        }
     }
 }
