fed9a74727fcfb3c2ea9b21fd5bd16bd963c7909
Jeffrey Rennie <rennie@google.com>
2016-04-05 13:26:24 -0700

    SignalR loads without error.

---
+++ aspnet/5-pubsub/worker-mvc/Hubs/LogHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace worker_mvc.Hubs
+{
+    public class LogHub : Hub
+    {
+        public void LogVerbose(string message) => Clients.All.logVerbose(message);
+
+        public void LogError(string message, Exception e) => Clients.All.logError(message, e.ToString());
+    }
+}
