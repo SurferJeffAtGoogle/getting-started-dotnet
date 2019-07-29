88be1623c086569eafa9fa4065fdebe7845bd5cd
Jeffrey Rennie <rennie@google.com>
2019-07-29 14:11:31 -0700

    Set IISServerOptions in ConfigureServices.
    Following instructions here:
    https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/iis/?view=aspnetcore-2.2

--- HelloWorld/Startup.cs
+++ HelloWorld/Startup.cs
@@ -25,6 +25,10 @@ namespace HelloWorld
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<IISServerOptions>(options => 
+            {
+                options.AutomaticAuthentication = false;
+            });        
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
